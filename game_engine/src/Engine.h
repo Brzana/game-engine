a0f7c96e05e9d667599b1294c95bbb19b1a002c5
Brzana <szymonbrzana@gmail.com>
2024-06-11 18:30:33 +0200

    setup basic app and entry point

---
+++ game_engine/src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by engine apps
+//You have to change macro in sandbox to access this file because visual studio couldn't hadle dynamic macro (idk why)
+
+#include "engine\Application.h"
+
+// entry point
+#include "engine\EntryPoint.h"
+// entry point
---
+++ game_engine/src/engine/EntryPoint.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+#ifdef ENGINE_PLATFORM_WINDOWS
+
+extern Engine::Application* Engine::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	auto app = Engine::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+
+#else
+#error "ENGINE only supports Windows"
+#endif
